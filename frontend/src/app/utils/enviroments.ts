ceb260397957c5dd9c48786b345a43b8aeb24c35
mrrushikeshdaund <rushikeshdaund@yahoo.com>
2025-08-29 20:17:24 +0530

    remove the application

--- frontend/src/app/utils/enviroments.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: false,
-  baseUrl: 'http://localhost:3000/',
-  users: {
-    register: 'users/register',
-    login: 'users/login',
-    profile: 'users/profile',
-  },
-};
